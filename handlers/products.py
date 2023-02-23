71e43ca26c7906c456550588670475c80f783c30
beka0708 <abdulasanovbektur@gmail.com>
2023-02-23 13:42:29 +0600

    'hw4'

--- handlers/products.py
+++ handlers/products.py
@@ -1,56 +1,56 @@
-from aiogram import types
-import random
-from config import bot
-
-kb = types.ReplyKeyboardMarkup()
-kb.add(
-    types.KeyboardButton("Посмотреть гаджеты")
-)
-kb.add(
-    types.KeyboardButton('Узнать цену')
-)
-kb.add(
-    types.KeyboardButton('Купить гаджет')
-)
-kb.add(
-    types.KeyboardButton('Адрес')
-)
-
-
-async def show_products(message: types.Message):
-    chat_id = message.from_user.id
-    await message.answer(
-        text="Hello",
-        reply_markup=kb
-    )
-
-
-async def show_see(message: types.Message):
-    ph = (
-        'imagess/divan.jpg',
-        'imagess/razmer.jpg',
-        'imagess/shkaf.jpg'
-    )
-    photo = open(random.choice(ph), 'rb')
-    await message.answer_photo(
-        photo=photo,
-        caption='Уникальные гаджеты',
-        reply_markup=kb
-    )
-
-
-async def show_price(message: types.Message):
-    await message.reply('Летающий диван - 1000$\n'
-                        'Пальчиковый измерятель - 1500$\n'
-                        'Складыватель вещей - 2000$')
-
-
-async def show_shop(message: types.Message):
-    await message.answer('К сожелению у нас не осталось товаров в наличии((\nВ скором времени появятся!!!')
-
-
-async def show_address(message: types.Message):
-    await message.reply('Кыргызская Республика 720004\n'
-                        'г. Бишкек, ул. Калык Акиева №83\n'
-                        'Тел.: +996 (709)86-39-06\n'
-                        'email: jarvis@0708gmail.com')
+# from aiogram import types
+# import random
+# from config import bot
+#
+# kb = types.ReplyKeyboardMarkup()
+# kb.add(
+#     types.KeyboardButton("Посмотреть гаджеты")
+# )
+# kb.add(
+#     types.KeyboardButton('Узнать цену')
+# )
+# kb.add(
+#     types.KeyboardButton('Купить гаджет')
+# )
+# kb.add(
+#     types.KeyboardButton('Адрес')
+# )
+#
+#
+# async def show_products(message: types.Message):
+#     chat_id = message.from_user.id
+#     await message.answer(
+#         text="Hello",
+#         reply_markup=kb
+#     )
+#
+#
+# async def show_see(message: types.Message):
+#     ph = (
+#         'imagess/divan.jpg',
+#         'imagess/razmer.jpg',
+#         'imagess/shkaf.jpg'
+#     )
+#     photo = open(random.choice(ph), 'rb')
+#     await message.answer_photo(
+#         photo=photo,
+#         caption='Уникальные гаджеты',
+#         reply_markup=kb
+#     )
+#
+#
+# async def show_price(message: types.Message):
+#     await message.reply('Летающий диван - 1000$\n'
+#                         'Пальчиковый измерятель - 1500$\n'
+#                         'Складыватель вещей - 2000$')
+#
+#
+# async def show_shop(message: types.Message):
+#     await message.answer('К сожелению у нас не осталось товаров в наличии((\nВ скором времени появятся!!!')
+#
+#
+# async def show_address(message: types.Message):
+#     await message.reply('Кыргызская Республика 720004\n'
+#                         'г. Бишкек, ул. Калык Акиева №83\n'
+#                         'Тел.: +996 (709)86-39-06\n'
+#                         'email: jarvis@0708gmail.com')
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import executor
 from aiogram.dispatcher.filters import Text
+from config import dp
 import logging
-from config import dp, bot
-from handlers.admin import (
-    check_user_is_admin,
-    check_bad_words,
-    ban_user,
-    yes_no
+
+from handlers.user_info_fsm import (
+    UserForm,
+    start_user_dialog,
+    process_age,
+    process_name,
+    process_address,
+    process_day,
+    mail,
+    not_mail
 )
-
-logging.basicConfig(level=logging.INFO)
-
-if __name__ == '__main__':
-    print('good')
-
-dp.register_message_handler(check_user_is_admin)
-dp.register_message_handler(check_bad_words)
-dp.register_message_handler(yes_no, commands=['да'], commands_prefix='!/')
-dp.register_message_handler(ban_user, commands=['ban'], commands_prefix='!/')
-
-executor.start_polling(dp)
+if __name__=="__main__":
+    print(__name__)
+    logging.basicConfig(level=logging.INFO)
+    dp.register_message_handler(start_user_dialog, commands=["form"])
+    dp.register_message_handler(process_name, state=UserForm.name)
+    dp.register_message_handler(process_age, state=UserForm.age)
+    dp.register_message_handler(process_address, state=UserForm.address)
+    dp.register_message_handler(process_day, state=UserForm.day)
+    dp.register_callback_query_handler(mail, Text(startswith="да"))
+    dp.register_callback_query_handler(not_mail, Text(startswith="нет"))
+    executor.start_polling(dp, skip_updates=True)
